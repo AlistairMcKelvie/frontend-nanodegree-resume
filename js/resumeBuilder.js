aad2b013c799c93b1b4b850443f41cddb31a5813
Alistair McKelvie <alistair.mckelvie@gmail.com>
2015-11-15 13:21:14 +1100

    feat: Remove old code and use JSON resume data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,88 @@
-$("#main").append(["Alistair McKelvie"]);
+var work = {
+    "jobs": [
+        {
+            "Employer": "Offshore Weather Services",
+            "Title": "Meteorologist",
+            "Location": "Melbourne, Australia",
+            "Dates": "December 2008 - Present"
+        },
+        {
+            "Employer": "Monash University Water Studies Centre",
+            "Title": "Laboratory Technician",
+            "Location": "Melbourne, Australia",
+            "Dates": "January 2005 - December 2008"
+        }
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "Online Portfolio",
+            "dates": "October 2015 - November 2015",
+            "description": "An online portfolio for Udacity Front End Developer Nanodegree",
+            "images": "images/fry.jpg"
+        }
+    ]
+};
+
+var bio = {
+    "name": "Alistair McKelvie",
+    "role": "Front end developer",
+    "contact": {
+        "email": "alistair.mckelvie@gmail.com",
+        "github": "github.com/AlistairMcKelvie",
+        "mobile": "+611 415 896 684",
+        "location": "Melbourne, Australia"
+    },
+    "picture": "images/fry.jpg",
+    "welcomeMessage": "Greetings!",
+    "skills": [
+        "Python",
+        "HTML/CSS",
+        "Javascript"
+    ]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Australian Bureau of Meteology Training Centre",
+            "city": "Melbourne, Australia",
+            "degree": "Graduate Diploma of Meteorology"
+        },
+        {
+            "name": "Monash University",
+            "city": "Melbourne, Australia",
+            "degree": "Bachelor of Science",
+            "major": "Applied Mathematics",
+            "minor": "Chemistry"
+        },
+        {
+            "name": "Luther College",
+            "city": "Melbourne, Australia",
+            "degree": "Victorian Certificate of Education",
+            "ENTER": 97.05
+        }
+    ],
+    "onlineCourses": [
+        {
+            "name": "Algorithms: Design: and Analysis, Part 1",
+            "school": "Stanford University",
+            "date": "December 14, 2014"
+        },
+        {
+            "name": "Algorithms: Design: and Analysis, Part 2",
+            "school": "Stanford University",
+            "date": "May 29, 2015"
+        },
+        {
+            "name": "Cryptography 1",
+            "school": "Stanford University",
+            "date": "November 11, 2014"
+        }
+    ]
+};
+
+
+
